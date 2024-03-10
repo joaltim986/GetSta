96abeeb41acbe30153959514a5eb366ce961e806
Jose Tintaya <joaltima986@gmail.com>
2024-03-09 20:19:45 -0500

    Added Features

---
+++ app.js
@@ -0,0 +1,11 @@
+var msg = 'Hello World 1';
+console.log(msg);
+var msg1 = 'Hello World 2';
+console.log(msg1);
+var msg2 = 'Hello World 3';
+console.log(msg2);
+var msg3 = 'Hello World 4';
+console.log(msg3);
+var msg4 = 'Hello World 5';
+console.log(msg4);
+
---
+++ pythunSample.py
@@ -0,0 +1,4 @@
+print('----------------------------------')
+print('Python has entred the building')
+print('Ley  the party star!')
+print('----------------------------------')
